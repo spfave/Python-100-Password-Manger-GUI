51c89ceb656baeb245a3f26a78dee14cce8f6d2d
sfave <sebpfave@gmail.com>
2020-12-05 17:31:06 -0500

    Canvas with logo

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+from tkinter import *
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- PASSWORD GENERATOR --------------------- #
+
+
+# ---------------------------- SAVE PASSWORD -------------------------- #
+
 
 # ---------------------------- UI SETUP ------------------------------- #
+root = Tk()
+root.title("Password Manager")
+root.config(padx=20, pady=20)
+
+
+canvas_logo = Canvas(width=200, height=200)
+image_logo = PhotoImage(file="images/logo.png")
+# default anchor is "center"
+canvas_logo.create_image(100, 100, image=image_logo, anchor="center")
+canvas_logo.pack()
+
+
+root.mainloop()
