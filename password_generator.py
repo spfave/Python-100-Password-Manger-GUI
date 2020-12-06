ece4e732e47f35bf9249c2f8ff3dcdac2bbd7ea4
sfave <sebpfave@gmail.com>
2020-12-05 19:22:54 -0500

    Refactor password_generator

--- password_generator.py
+++ password_generator.py
@@ -1,29 +1,33 @@
 # Password Generator Project
 import random
+
+
+# Constants
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
            'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-nr_letters = random.randint(8, 10)
-nr_symbols = random.randint(2, 4)
-nr_numbers = random.randint(2, 4)
 
-password_list = []
+# Functions
+def generate_password():
+    """  """
 
-for char in range(nr_letters):
-    password_list.append(random.choice(letters))
+    nr_letters = random.randint(8, 10)
+    nr_symbols = random.randint(2, 4)
+    nr_numbers = random.randint(2, 4)
 
-for char in range(nr_symbols):
-    password_list += random.choice(symbols)
+    password_list = []
+    for _ in range(nr_letters):
+        password_list.append(random.choice(letters))
 
-for char in range(nr_numbers):
-    password_list += random.choice(numbers)
+    for _ in range(nr_symbols):
+        password_list.append(random.choice(symbols))
 
-random.shuffle(password_list)
+    for _ in range(nr_numbers):
+        password_list.append(random.choice(numbers))
 
-password = ""
-for char in password_list:
-    password += char
+    random.shuffle(password_list)
+    password = "".join(password_list)
 
-print(f"Your password is: {password}")
+    print(f"Your password is: {password}")
